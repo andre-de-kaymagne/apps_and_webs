9bfd92ae837c580cc983f76a731fe12265815fa3
andre-de-kaymagne <129093283+andre-de-kaymagne@users.noreply.github.com>
2023-06-02 11:06:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/*console.log(document.querySelector('.page'));
+console.log(document.querySelector('.theme-button'));*/
+
+
+/*document.querySelector('.page').classList.remove('light-theme'); //убираем класс у элемента
+document.querySelector('.page').classList.add('dark-theme'); //добавляем класс элементу*/
+
+//let variableName = 'Я значение переменной!';
+
+/*let header = document.querySelector('header');
+console.log(header);
+header.classList.add('new-class');*/
+
+/*let page = document.querySelector('.page');
+page.classList.remove('light-theme');
+page.classList.add('dark-theme');*/
+
+/*let button = document.querySelector('button');
+button.onclick = function() {
+  console.log('Клик!');
+};*/
+
+/*let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+  console.log('Кнопка нажата!');
+};*/
+
+/*let themeButton = document.querySelector('.theme-button');
+themeButton.onclick = function() {
+  console.log('Кнопка нажата!');
+  page.classList.remove('light-theme');
+  page.classList.add('dark-theme');
+  
+};*/
+
+/*let page = document.querySelector('.page');
+let themeButton = document.querySelector('.theme-button');
+
+themeButton.onclick = function() {
+  page.classList.toggle('light-theme');
+  page.classList.toggle('dark-theme');
+  
+};*/ //переключение темной и светлой темы у боди
